dba1c25ae3a6dc0d01b07238d15adf2a87a51bcf
Subrata Sarkar <tccdcc8@gmail.com>
2024-08-09 18:51:14 +0600

    m

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,27 +1,11 @@
 import Features from "../../Components/HomePageCom/Features";
+import Hero from "../../Components/HomePageCom/Hero";
 import Testimonials from "../../Components/HomePageCom/Testimonials";
 
 const Home = () => {
   return (
     <div className="text-center">
-      <section className="bg-blue-500 text-white text-center py-20">
-        <h1 className="text-4xl font-bold mb-4">
-          Find Your Perfect Rental Home
-        </h1>
-        <p className="text-xl mb-8">
-          Browse through the best rental properties available
-        </p>
-        <div className="max-w-lg mx-auto">
-          <input
-            type="text"
-            placeholder="Search by location"
-            className="p-2 rounded w-full mb-4"
-          />
-          <button className="bg-white text-blue-500 px-4 py-2 rounded">
-            Search
-          </button>
-        </div>
-      </section>
+      <Hero />
       <Testimonials />
       <Features />
     </div>
